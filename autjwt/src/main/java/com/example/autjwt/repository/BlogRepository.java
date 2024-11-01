71f8ae09e1af151c3f154b5b93079565ce4828a0
Vivaldi Afonso Jr <147620225+Vivaldi-Dev@users.noreply.github.com>
2024-11-01 09:14:20 +0200

    Delete autjwt directory

--- autjwt/src/main/java/com/example/autjwt/repository/BlogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.autjwt.repository;
-
-import com.example.autjwt.entity.Blog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlogRepository extends JpaRepository<Blog, Long> {
-}
