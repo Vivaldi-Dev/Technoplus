821ec6750f693d1516c65dd52204d34844628c41
Vivaldi Afonso Jr <147620225+Vivaldi-Dev@users.noreply.github.com>
2024-11-01 09:32:55 +0200

    Delete autjwt directory

--- autjwt/src/main/java/com/example/autjwt/dto/RegisterDTO.java
+++
@@ -1,7 +0,0 @@
-package com.example.autjwt.dto;
-
-
-import com.example.autjwt.entity.UserRoles;
-
-public record RegisterDTO(String login, String password, UserRoles role ,String name) {
-}
--- autjwt/src/main/java/com/example/autjwt/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.autjwt.repository;
-
-import com.example.autjwt.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
